b8175bff1d801420af9df903874f00aedb4ee918
LoukasGP <team@hiredinchina.org>
2021-12-30 11:16:28 +0000

    more changes to the swagger file. Need someone else to look through it

--- db/index.js
+++ db/index.js
@@ -1,6 +1,10 @@
 const { Pool } = require('pg')
 const { DB } = require('../config')
 
+/**
+ * Created a new pool to access the environment variables
+ */
+
 const pool = new Pool ({
     user: DB.PGUSER,
     host: DB.PGHOST,
