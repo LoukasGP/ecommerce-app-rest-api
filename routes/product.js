bebc10c19a5f441fde4522291bf88f50b835e4c3
LoukasGP <team@hiredinchina.org>
2022-01-11 16:24:19 +0000

    product section finalised with validation, controllers, routes, and services

--- routes/product.js
+++ routes/product.js
@@ -1,75 +1,103 @@
 const express = require("express");
-const productRouter = express.Router();
-const { pool } = require("../config");
-const addItem = require("../services/productServices");
-const { findByID } = require("../services/productServices");
-productRouter.use(express.json({ extended: false }));
+const passport = require("passport");
+const Router = require("express-promise-router");
+const {
+  validateGetProducts,
+  validatePostProduct,
+  validatePutProduct,
+  validateDeleteProduct,
+} = require("./validation/validation");
+const { products } = require("../controllers");
+const productRouter = new Router();
 
-//TODO ADD CREATED AND MODIFIED FIELDS
-productRouter.post("/", (req, res) => {
-  const { id, name, description, quantity } = req.body;
-  pool.query(
-    "INSERT INTO product(id, name, description, quantity) VALUES($1, $2, $3, $4)",
-    [id, name, description, quantity]
+productRouter
+  .get("/", products.getAllProducts)
+  .get("/:id", validateGetProducts, products.getProductById)
+  .post(
+    "/",
+    validatePostProduct,
+    passport.authenticate("jwt-admin", { session: false }),
+    products.postProduct
+  )
+  .put(
+    "/:id",
+    validatePutProduct,
+    passport.authenticate("jwt-admin", { session: false }),
+    products.putProduct
+  )
+  .delete(
+    "/:id",
+    validateDeleteProduct,
+    passport.authenticate("jwt-admin", { session: false }),
+    products.deleteProduct
   );
-});
 
-//READ
-productRouter.get("/:id", async (req, res) => {
-  await pool.query(
-    "SELECT * FROM product WHERE id = $1",
-    [req.params.id],
-    (err, data) => {
-      if (err) res.status(400).send(err);
-      if (data.rowCount > 0) res.status(200).send(data);
-      return res.status(422).json({
-        error: {
-          attemptedProductID: `Requested Product: ${req.params.id}`,
-          status: 417,
-          data: "No product with this id",
-        },
-      });
-    }
-  );
-});
+// //TODO ADD CREATED AND MODIFIED FIELDS
+// productRouter.post("/", (req, res) => {
+//   const { id, name, description, quantity } = req.body;
+//   pool.query(
+//     "INSERT INTO product(id, npm run devname, description, quantity) VALUES($1, $2, $3, $4)",
+//     [id, name, description, quantity]
+//   );
+//   //TODO what if data is input incorrectly?
+// });
 
-//TODO - FIX ERROR HEADER SET ALREADY
-productRouter.get("/", (req, res) => {
-  pool.query("SELECT * FROM product WHERE quantity > 0;", (err, data) => {
-    if (err) res.status(400).send(err);
-    if (data.rowCount > 0) res.status(200).send(data);
-    return res.status(422).json({
-      error: {
-        status: 417,
-        data: "No Products to show",
-      },
-    });
-  });
-});
+// //READ
+// productRouter.get("/:id", async (req, res) => {
+//   await pool.query(
+//     "SELECT * FROM product WHERE id = $1",
+//     [req.params.id],
+//     (err, data) => {
+//       if (err) res.status(400).send(err);
+//       if (data.rowCount > 0) res.status(200).send(data);
+//       return res.status(422).json({
+//         error: {
+//           attemptedProductID: `Requested Product: ${req.params.id}`,
+//           status: 417,
+//           data: "No product with this id",
+//         },
+//       });
+//     }
+//   );
+// });
 
-//UPDATE
-productRouter.put("/:id", async (req, res) => {
-  const newProduct = req.body;
-  const productId = Number(req.params.id);
-  const products = await pool.query("SELECT * FROM product;");
-  console.log(products);
-  if (!newProduct.id || newProduct.id !== productId) {
-    newProduct.id = productId;
-  }
-  products[productId] = newProduct;
-  res.send(newProduct);
-});
+// //TODO - FIX ERROR HEADER SET ALREADY
+// productRouter.get("/", async (req, res) => {
+//   await pool.query("SELECT * FROM product WHERE quantity > 0;", (err, data) => {
+//     if (err) res.status(400).send(err);
+//     if (data.rowCount > 0) res.status(200).send(data);
+//     return res.status(422).json({
+//       error: {
+//         status: 417,
+//         data: "No Products to show",
+//       },
+//     });
+//   });
+// });
 
-//DELETE
-productRouter.delete("/:id", async(req, res) => {
-  const productId = Number(req.params.id);
-  const products = await pool.query("SELECT * FROM product;");
-  if (productId !== -1) {
-    products.splice(productId, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).send();
-  }
-});
+// //UPDATE
+// productRouter.put("/:id", async (req, res) => {
+//   const newProduct = req.body;
+//   const productId = Number(req.params.id);
+//   const products = await pool.query("SELECT * FROM product;");
+//   console.log(products);
+//   if (!newProduct.id || newProduct.id !== productId) {
+//     newProduct.id = productId;
+//   }
+//   products[productId] = newProduct;
+//   res.send(newProduct);
+// });
+
+// //DELETE
+// productRouter.delete("/:id", async(req, res) => {
+//   const productId = Number(req.params.id);
+//   const products = await pool.query("SELECT * FROM product;");
+//   if (productId !== -1) {
+//     products.splice(productId, 1);
+//     res.status(204).send();
+//   } else {
+//     res.status(404).send();
+//   }
+// });
 
 module.exports = productRouter;
--- services/productServices.js
+++ services/productServices.js
@@ -1,16 +1,41 @@
-// loads all the products
-// checks if the product exists
-// loads individual product
-// checks quantity to see if they can add it to the cart
+const { pool } = require("../config");
 
-const validateProduct = (req, res, next) => {};
-
-//check the product they're adding is unique for the post request
-const productIsUnique = (req, res, next) => {
-  const { id } = req.body;
-  pool.query("SELECT * FROM product WHERE id = $1 ", [id], (err, data) =>
-    console.log(err)
-  );
+const fetchProducts = async () => {
+  const res = await pool.query("SELECT * FROM product;");
+  return res.rows;
 };
 
-module.exports = { validateProduct, productIsUnique };
+const fetchProductById = async (id) => {
+  const res = await pool.query("SELECT * FROM product WHERE id = $1;", [id]);
+  return res.rows;
+};
+
+const createProduct = async ({ id, name, description, quantity }) => {
+  const text = `INSERT INTO product(id, name, description, quantity)
+                VALUES($1, $2, $3, $4) RETURNING *`;
+  const values = [id, name, description, quantity];
+  const res = await pool.query(text, values);
+  return res.rows;
+};
+
+const modifyProduct = async ({ id, name, description, quantity }) => {
+  const text = `UPDATE product SET name=$2, description=$3, quantity=$4
+    WHERE id = $1 RETURNING *`;
+  const values = [id, name, description, quantity];
+
+  const res = await pool.query(text, values);
+  return res.rows;
+};
+
+const removeProduct = async (id) => {
+  const res = await pool.query("DELETE FROM product WHERE id = $1;", [id]);
+  return res.rows;
+};
+
+module.exports = {
+  fetchProducts,
+  fetchProductById,
+  createProduct,
+  modifyProduct,
+  removeProduct,
+};
